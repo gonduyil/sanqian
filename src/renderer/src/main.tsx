b7ad43ba3b95065277f1797e9561b0d7f5d8d8ea
liyd <1768756152@qq.com>
2025-09-18 11:47:06 +0800

    feat: install antd

---
+++ src/renderer/src/main.tsx
@@ -0,0 +1,12 @@
+import './assets/main.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import 'antd/dist/reset.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
